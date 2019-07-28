aef7505ea33dbe282a181bf911a76387bf4f3080
กฤตกร ใคร่ครวญ <kittakornkraikruan@hotmail.com>
2019-07-28 15:38:44 +0700

    28/7/2562

---
+++ Travel/TravelApp/TravelApp/Controls/FlatNavigationPage.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace TravelApp.Controls
+{
+    public class FlatNavigationPage : NavigationPage
+    {
+        public static BindableProperty LogoProperty = BindableProperty.Create(nameof(Logo), typeof(string), typeof(FlatNavigationPage), null);
+
+        public string Logo
+        {
+            get => (string)GetValue(LogoProperty);
+            set => SetValue(LogoProperty, value);
+        }
+
+        public FlatNavigationPage(Page root)
+            : base(root)
+        {
+            this.BarBackgroundColor = Color.FromHex("#2196F3");
+            this.BarTextColor = Color.White;
+        }
+    }
+}
